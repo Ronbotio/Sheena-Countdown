9ad1a6810c9b1b6bec4e0cd2a868692f7bac0a74
Ronbotio <ronaldantoinejr@gmail.com>
2025-11-26 17:07:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -53,6 +53,9 @@ const MESSAGES = [
     "Just like Eliza Doolittle's dream of a better life. Keep storing up treasures above. Oh when you arrive at your heavenly home 'Would'nt It Be Loverly?'",
     "I was just listening to 'Christian's Song' by Torey Dshaun and it made me think of the time on our way home from the BP O4W conference listening to 'Come and See' album in your car. Thank you for the bop! *starts dancing🕺🏾",
     "Eh......at your wedding we may need to add Alfred Doolittle's 'With a bit of luck' song. So we can.....I mean so I can dance. Lol"
+    "You’re not just growing older; you’re growing into a more magnificent version of the woman God designed you to be. Keep shining!"
+    "May this next year be filled with as much grace, revelation, and excellent food runs as the last! Happy Birthday season!"
+
 ];
 
 // --- 2. CIPHER GAME ARRAYS ---
@@ -389,6 +392,7 @@ function markMessagesAsRead() {
 // Initialize the countdown when the page loads
 loadAnnouncements();
 initializeDailyContent();
+
 
 
 
